018f9b381d1f56f6e64fbd68c6defcb1e147199c
k-minsik <kims_______@naver.com>
2024-04-08 17:33:10 +0900

    feat: Create Signaling Server

--- src/main/java/meritz/websocket/WebSocketConfig.java
+++ src/main/java/meritz/websocket/WebSocketConfig.java
@@ -12,14 +12,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        // broker uri 설정
         config.enableSimpleBroker("/topic");
+        // send uri 설정
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // webSocket 접속 시, endpoint 설정
         registry.addEndpoint("/signaling")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*") // cors에 따른 설정( *는 모두 허용 )
+                .withSockJS(); // 브라우저에서 webSocket을 지원하지 않는 경우의 대안
     }
 }
