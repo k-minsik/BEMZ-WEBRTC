2b38f7a49e50e043a7b779dd67d2a9d29e0ae70d
k-minsik <kims_______@naver.com>
2024-04-08 16:30:13 +0900

    init: Create project

---
+++ src/main/java/meritz/BemzApplication.java
@@ -0,0 +1,13 @@
+package meritz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BemzApplication.class, args);
+    }
+
+}
